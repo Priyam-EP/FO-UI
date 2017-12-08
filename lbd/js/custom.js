1cf397f0be1b605f9b8919b2cc4b0d041097b908
kkumarasamy <kkumarasamy@edit-place.com>
2017-12-08 15:24:40 +0530

    Update FO royalties changes

--- lbd/js/custom.js
+++ lbd/js/custom.js
@@ -444,14 +444,11 @@ $(document).ready(function(){
     });
 
 
-<<<<<<< HEAD
-=======
     //contract Tag
 /*	$('#contract-tags').tagEditor({
         placeholder: '+ Add a tag',
         autocomplete: { minLength: 3, delay: 250, html: true, position: { collision: 'flip' } }
     });*/
->>>>>>> c91b1d8024bfde9e98a583e086f73e6599ed019d
 
 
 	//Tab Edit content toggle
