fb3b7f98cdd4a158d4d61b34240877210d33fe5b
kkumarasamy <kkumarasamy@edit-place.com>
2017-12-15 20:23:13 +0530

    Update cards-list

--- builds/development/lbd/js/custom.js
+++ builds/development/lbd/js/custom.js
@@ -648,8 +648,8 @@ $(document).ready(function(){
 
 
 
-	//Readmore/less Code
-	// Configure/customize these variables.
+  //Readmore/less Code
+  // Configure/customize these variables.
     var showChar = 250;  // How many characters are shown by default
     var ellipsestext = "...";
     var moretext = "See more";
@@ -658,7 +658,7 @@ $(document).ready(function(){
 
      $('.more').each(function() {
         var content = $(this).html(),
-        		textContent = $(this).text();
+            textContent = $(this).text();
         // console.log(content);
         if(content.length > showChar) {
             var c = content.substr(0, showChar);
@@ -671,18 +671,6 @@ $(document).ready(function(){
         }
     });
 
-/*$(".morelink").click(function(){
-    if($(this).hasClass("less")) {
-        $(this).removeClass("less");
-        $(this).html(moretext);
-    } else {
-        $(this).addClass("less");
-        $(this).html(lesstext);
-    }
-    $(this).parent().prev().toggle();
-    $(this).prev().toggle();
-    return false;
-});*/
     $(".morelink").click(function(e){
         e.stopPropagation();
         if($(this).hasClass("less")) {
@@ -697,6 +685,57 @@ $(document).ready(function(){
         return false;
     });
 
+
+(function($) {
+  $.fn.shorten = function (settings) {
+
+    var config = {
+      showChars: 100,
+      ellipsesText: "...",
+      moreText: "more",
+      lessText: "less"
+    };
+
+    if (settings) {
+      $.extend(config, settings);
+    }
+
+    $(document).off("click", '.morelink');
+
+    $(document).on({click: function () {
+
+        var $this = $(this);
+        if ($this.hasClass('less')) {
+          $this.removeClass('less');
+          $this.html(config.moreText);
+        } else {
+          $this.addClass('less');
+          $this.html(config.lessText);
+        }
+        $this.parent().prev().toggle();
+        $this.prev().toggle();
+        return false;
+      }
+    }, '.morelink');
+
+    return this.each(function () {
+      var $this = $(this);
+      if($this.hasClass("shortened")) return;
+
+      $this.addClass("shortened");
+      var content = $this.html();
+      if (content.length > config.showChars) {
+        var c = content.substr(0, config.showChars);
+        var h = content.substr(config.showChars, content.length - config.showChars);
+        var html = c + '<span class="moreellipses">' + config.ellipsesText + ' </span><span class="morecontent"><span>' + h + '</span> <a href="#" class="morelink">' + config.moreText + '</a></span>';
+        $this.html(html);
+        $(".morecontent span").hide();
+      }
+    });
+
+  };
+
+ })(jQuery);
 
 
     //Full Calendar
